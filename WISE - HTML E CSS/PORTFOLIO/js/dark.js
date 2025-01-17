b1125b4d9fc6eb71c4b192e58a1ccc3aca151566
Tiago Gorri <155651809+tiagogorridev@users.noreply.github.com>
2025-01-17 00:53:45 -0300

    🗑️ Removi imagens e animações não utilizadas 🗑️ Removi a animação da seção de introdução quando o usuário entrava no site 🌙 Comecei a adicionar suporte a modo escuro e alternador de tema 🎨 Melhorei o CSS para suportar o modo escuro 📁 Removi o botão do WhatsApp no header e migrei para ao lado dos botões na seção de introdução 📁 Coloquei uma imagem de lua no local antigo do símbolo do WhatsApp no header para poder realizar a mudança entre o tema claro e escuro

---
+++ WISE - HTML E CSS/PORTFOLIO/js/dark.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const themeToggle = document.getElementById("theme-toggle");
+    const body = document.body;
+    const themeImage = document.getElementById("logo");
+    const moonIcon = document.getElementById("theme-toggle");
+    const savedTheme = localStorage.getItem("theme");
+    if (savedTheme) {
+        body.classList.toggle("dark-mode", savedTheme === "dark");
+        if (savedTheme === "dark") {
+            themeImage.src = "../assets/img/logo-dark.png";
+            themeImage.alt = "Imagem do Tema Escuro";
+            moonIcon.src = "../assets/img/lua-dark.png";
+            moonIcon.alt = "Ícone do Tema Escuro";
+        }
+    }
+
+    themeToggle.addEventListener("click", () => {
+        const isDark = body.classList.toggle("dark-mode");
+        localStorage.setItem("theme", isDark ? "dark" : "light");
+        if (isDark) {
+            themeImage.src = "../assets/img/logo-dark.png";
+            themeImage.alt = "Imagem do Tema Escuro";
+            moonIcon.src = "../assets/img/lua-dark.png";
+            moonIcon.alt = "Ícone do Tema Escuro";
+        } else {
+            themeImage.src = "../assets/img/logo.png";
+            themeImage.alt = "Imagem do Tema Claro";
+            moonIcon.src = "../assets/img/lua.png";
+            moonIcon.alt = "Ícone do Tema Claro";
+        }
+    });
+});
